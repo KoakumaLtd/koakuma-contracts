5d1434f3d01fa0ac8d9f931edea1bdcdf01d89d3
Koakuma Ltd <admin@koakuma.io>
2023-01-15 01:21:33 +0800

    feat: Update Koakuma Token, add anti-bot

--- contracts/Koakuma.sol
+++ contracts/Koakuma.sol
@@ -5,12 +5,21 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+interface IBPContract {
+    function protect(
+        address sender,
+        address receiver,
+        uint256 amount
+    ) external;
+}
 
 /// @custom:security-contact admin@koakuma.io
 contract Koakuma is ERC20, ERC20Burnable, Pausable, Ownable {
-    using SafeMath for uint256;
-    uint256 private _tax;
+    IBPContract public bpContract;
+
+    bool public bpEnabled;
+    bool public bpDisabledForever;
 
     constructor() ERC20("Koakuma", "KKMA") {
         _mint(msg.sender, 1000000000 * 10**decimals());
@@ -24,49 +33,31 @@ contract Koakuma is ERC20, ERC20Burnable, Pausable, Ownable {
         _unpause();
     }
 
+    function setBPContract(address addr) public onlyOwner {
+        require(addr != address(0), "BP address cannot be 0x0");
+
+        bpContract = IBPContract(addr);
+    }
+
+    function setBPEnabled(bool enabled) public onlyOwner {
+        bpEnabled = enabled;
+    }
+
+    function setBPDisableForever() public onlyOwner {
+        require(!bpDisabledForever, "Bot protection disabled");
+
+        bpDisabledForever = true;
+    }
+
     function _beforeTokenTransfer(
         address from,
         address to,
         uint256 amount
     ) internal override whenNotPaused {
+        if (bpEnabled && !bpDisabledForever) {
+            bpContract.protect(from, to, amount);
+        }
+
         super._beforeTokenTransfer(from, to, amount);
-    }
-
-    function setTax(uint256 tax_) public onlyOwner {
-        require(tax_ <= 10000, "Koakuma: tax exceeds 100%");
-        _tax = tax_;
-    }
-
-    function getTax() public view returns (uint256) {
-        return _tax;
-    }
-
-    function transferFrom(
-        address sender,
-        address recipient,
-        uint256 amount
-    ) public override returns (bool) {
-        uint256 taxFee = (amount.mul(_tax)).div(10000);
-
-        if (taxFee > 0) {
-            _transfer(sender, address(this), taxFee);
-        }
-        _transfer(sender, recipient, amount.sub(taxFee));
-
-        uint256 currentAllowance = allowance(sender, _msgSender());
-        require(
-            currentAllowance >= amount,
-            "ERC20: transfer amount exceeds allowance"
-        );
-        unchecked {
-            _approve(sender, _msgSender(), currentAllowance - amount);
-        }
-
-        return true;
-    }
-
-    function withdrawTax(address recipient_, uint256 amount_) public onlyOwner {
-        IERC20 tokenContract = IERC20(this);
-        tokenContract.transfer(recipient_, amount_);
     }
 }
